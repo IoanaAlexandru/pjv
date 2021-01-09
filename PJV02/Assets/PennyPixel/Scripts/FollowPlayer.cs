cb1300a82d2250ed68b0e1a31815f3218a56c03b
Ioana Alexandru <ioanaa.alexandru98@gmail.com>
2021-01-09 16:25:47 +0200

    [lab 2] Make background follow player

---
+++ PJV02/Assets/PennyPixel/Scripts/FollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] Vector3 offset = new Vector3(0, 0, 0);
+
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y+offset.y, transform.position.z + offset.z);
+    }
+}

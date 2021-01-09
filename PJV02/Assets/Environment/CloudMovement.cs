21341ffe61e43d1f7373c8f86a00570adce34fc3
Ioana Alexandru <ioanaa.alexandru98@gmail.com>
2021-01-09 22:15:50 +0200

    [lab 2] Animate clouds

---
+++ PJV02/Assets/Environment/CloudMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour
+{
+    float scale = 1f;
+    [SerializeField] bool growing = true;
+    [SerializeField] int scaleSpeed = 5;
+
+    void Update()
+    {
+        if (growing)
+        {
+            scale += 0.01f * scaleSpeed * Time.deltaTime;
+        } else
+        {
+            scale -= 0.01f * scaleSpeed * Time.deltaTime;
+        }
+
+        if (scale < 0.95 || scale > 1.05)
+        {
+            growing = !growing;
+        }
+
+        transform.localScale = new Vector2(scale, scale);
+    }
+}

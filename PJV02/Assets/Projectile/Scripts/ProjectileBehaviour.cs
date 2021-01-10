831ecff6e69d5115a7e5c81160d81f3315948611
Ioana Alexandru <ioanaa.alexandru98@gmail.com>
2021-01-10 19:56:32 +0200

    [lab 2] Add zombie enemy and projectile prefabs

---
+++ PJV02/Assets/Projectile/Scripts/ProjectileBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ProjectileBehaviour : MonoBehaviour
+{
+    public float speed = 3;
+    public bool movingLeft = true;
+
+    bool hitSomething = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!movingLeft)
+        {
+            var spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hitSomething)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime * (movingLeft ? -1 : 1), Space.World);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Through"))
+        {
+            StartCoroutine(Disappear());
+        }
+    }
+
+    IEnumerator Disappear()
+    {
+        hitSomething = true;
+        var collider = gameObject.GetComponent<CapsuleCollider2D>();
+        collider.enabled = false;
+        for (float i = 1; i >= 0; i -= Time.deltaTime)
+        {
+            transform.localScale = transform.localScale * i;
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ PJV02/Assets/Zombie/Scripts/ZombieController.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ZombieController : PhysicsObject
+{
+
+    public float maxSpeed = 6;
+    public float leftLimit, rightLimit;
+    public float attackInterval = 1f;
+
+    public GameObject projectile;
+
+    CapsuleCollider2D zombieCollider;
+
+    bool movingLeft = true;
+    bool dead = false;
+    bool waiting = false;
+    bool attacking = false;
+
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    // Use this for initialization
+    void Awake()
+    {
+        zombieCollider = GetComponent<CapsuleCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        StartCoroutine(Attack());
+    }
+
+    protected override void ComputeVelocity()
+    {
+        if (dead || waiting || attacking)
+        {
+            targetVelocity = new Vector2(0, 0);
+            animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+            return;
+        }
+
+        var pos = gameObject.transform.position.x;
+        if (movingLeft && pos <= leftLimit)
+        {
+            StartCoroutine(WaitAndTurn());
+        }
+        else if (!movingLeft && pos >= rightLimit)
+        {
+            StartCoroutine(WaitAndTurn());
+        }
+
+        Vector2 move = new Vector2(movingLeft ? -1 : 1, 0);
+
+        if (move.x > 0.01f)
+        {
+            if (spriteRenderer.flipX == true)
+            {
+                spriteRenderer.flipX = false;
+            }
+        }
+        else if (move.x < -0.01f)
+        {
+            if (spriteRenderer.flipX == false)
+            {
+                spriteRenderer.flipX = true;
+            }
+        }
+
+        animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+
+        targetVelocity = move * maxSpeed;
+    }
+
+    IEnumerator WaitAndTurn()
+    {
+        waiting = true;
+        yield return new WaitForSeconds(1f);
+        waiting = false;
+        movingLeft = !movingLeft;
+    }
+
+    IEnumerator Attack()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(attackInterval);
+
+            if (dead)
+            {
+                break;
+            }
+
+            attacking = true;
+
+            // Trigger attack animation
+            animator.Play("Zombie_Attack");
+            var animationDuration = animator.GetCurrentAnimatorClipInfo(0).Length;
+
+            // Instantiate projectile
+            yield return new WaitForSeconds(animationDuration / 2);
+            ((ProjectileBehaviour)projectile.GetComponent<MonoBehaviour>()).movingLeft = movingLeft;
+            Instantiate(projectile, gameObject.transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(animationDuration / 2);
+
+            attacking = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D theCollider)
+    {
+        if (theCollider.CompareTag("Player"))
+        {
+            StartCoroutine(Die());
+        }
+    }
+
+    IEnumerator Die()
+    {
+        dead = true;
+        animator.Play("Zombie_Dead");
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorClipInfo(0).Length + 0.5f);
+        for (float i = 1; i >= 0; i -= Time.deltaTime)
+        {
+            spriteRenderer.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+}

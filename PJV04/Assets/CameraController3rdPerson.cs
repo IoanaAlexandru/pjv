b9464fbbeb22754d0eb6df6000bec84a46c56a3b
Ioana Alexandru <ioanaa.alexandru98@gmail.com>
2021-01-22 22:54:09 +0200

    [lab 3] Add FPS camera

---
+++ PJV04/Assets/CameraController3rdPerson.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController3rdPerson : MonoBehaviour
+{
+    Transform player;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        transform.position = player.position + offset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset;
+        transform.LookAt(player.position);
+    }
+}
---
+++ PJV04/Assets/GlobalCameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalCameraController : MonoBehaviour
+{
+    public Camera cam1;
+    public Camera cam3;
+
+    void Start()
+    {
+        cam1.enabled = false;
+        cam3.enabled = true;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            cam1.enabled = !cam1.enabled;
+            cam3.enabled = !cam3.enabled;
+        }
+    }
+}
